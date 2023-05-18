341ded4c6e6bd09980a318f986442c2a0412e17e
Sushant Pawar <131552558+SushantPawar-7007@users.noreply.github.com>
2023-05-18 10:59:19 +0530

    Add files via upload

---
+++ App6.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Homework5 from "./Homework5"
+import Details from "./Details";
+
+export default function App6()
+{
+    
+
+    return <div>
+
+       {/* <Homework5></Homework5> */}
+       <Details></Details>
+       
+      
+        
+    </div>
+}
